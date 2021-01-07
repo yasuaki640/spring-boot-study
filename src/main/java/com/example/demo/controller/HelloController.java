17348adba21adaa2f34e11a10a4eb7ba0392b676
yasuaki640 <yasuaki640@gmail.com>
2021-01-07 19:00:27 +0900

    receive get param

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
+        return "hello";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam("message") String message, Model model) {
+        model.addAttribute("sample", message);
         return "hello";
     }
 }
