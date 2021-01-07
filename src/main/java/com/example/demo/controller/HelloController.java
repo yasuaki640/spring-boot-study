6063fd6c1be65758bdbc4098d61d491186316d4e
yasuaki640 <yasuaki640@gmail.com>
2021-01-07 19:39:29 +0900

    change url mapping

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,18 +3,20 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping
     public String index() {
         return "hello";
     }
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam("message") String message, Model model) {
+    public String greeting(@RequestParam String message, Model model) {
         model.addAttribute("sample", message);
         return "hello";
     }
