3d7253855af68b12e6292cf7f6a3917b89690860
yasuaki640 <yasuaki640@gmail.com>
2021-01-07 22:26:39 +0900

    create form and confirm

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,24 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/form")
 public class HomeController {
 
-    @GetMapping
-    public String index() {
+    @GetMapping("/form")
+    private String readForm(@ModelAttribute User user) {
         return "form";
     }
 
-    @PostMapping("/confirm")
-    public String confirm(@RequestParam String message, Model model) {
-        model.addAttribute("message", message);
+    @PostMapping("/form")
+    private String confirm(@ModelAttribute User user) {
         return "confirm";
     }
 }
