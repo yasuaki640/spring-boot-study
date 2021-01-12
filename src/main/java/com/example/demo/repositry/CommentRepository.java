69382e11c962495b586f33240f99354ccba0256c
yasuaki640 <yasuaki640@gmail.com>
2021-01-12 21:11:40 +0900

    また更地に戻す

--- src/main/java/com/example/demo/repositry/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositry;
-
-import com.example.demo.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
